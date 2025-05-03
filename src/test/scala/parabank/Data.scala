57871a31a57b8c64799f1ad8cc722149e736f279
Leonardo Castro <44737382+lhcastrop@users.noreply.github.com>
2025-05-03 11:53:30 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,4 +4,8 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "john"
     val password = "demo"
+    val fromAccountId="15675"
+    val toAccountId= "15786"
+    val amount="1"    
+    
 }
